2ac81a17382001a65f676b268a8b437e9159119c
Shamoon Ahmed <samsonrizzy6@gmail.com>
2025-07-24 15:25:37 +0500

    type validation

--- main_pydantic.py
+++ main_pydantic.py
@@ -1,17 +1,21 @@
 from pydantic import BaseModel
 
+# Inheriting BaseModel for data validation
 class Patient(BaseModel):
     name : str
     age : int
 
+# Creating a function that accepts a Patient object. That patient object is defined later
 def insert_patient(patient : Patient):
     print("Patient's Name: ", patient.name)
     print("Patient's Age: ", patient.age)
     print("Patient Record Inserted!")
 
+# the new patient record. Pydantic will automatically convert string number into int if passed incorrectly passed
 patient_record = {'name':'Ali', 'age':20}
 
+# creating the patient object with patient record and then unpacking it
+# ** means it converts a dict into keyword arguments
 patient1 = Patient(**patient_record)
-print(patient1.name)
 
-# insert_patient(patient1)
+insert_patient(patient1)
