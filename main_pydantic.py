6c1ef0cafc336b5fdbdeaf49dff0d45f25b099e9
Shamoon Ahmed <samsonrizzy6@gmail.com>
2025-07-24 17:30:38 +0500

    type/data validation and some advance data types

--- main_pydantic.py
+++ main_pydantic.py
@@ -4,7 +4,7 @@ from typing import List, Dict, Annotated, Optional
 # Inheriting BaseModel for type and data validation
 class Patient(BaseModel):
     name : Annotated[str, Field(max_length=30, title='Name of Patient', description='Name registered in CNIC', examples='Rengoku')]
-    age : int = Field(gt=0, lt=110)
+    age : int = Field(gt=0, lt=110, strict=True) # As pydantic converts str numbers into int automatically, sometimes its not useful. So we can restrict a specific field to not convert a str number into in but saying strict=True. Now if we pass age as '20', the program will throw an error and will only work if correct int is passed in int format
     married : Annotated[Optional[bool], Field(default=False)] # If no value is passed, it defaults to False 
     linkedin_url : AnyUrl
     email: EmailStr
